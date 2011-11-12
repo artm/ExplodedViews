1f69811584566e619af4e9f95b743a6d22deeed4
Artem Baguinski <artm@v2.nl>
2011-11-12 12:14:30 +0100

    Don't collapse Clouds

--- Assets/Standard Assets/ExplodedViews/MakeTriggers.cs
+++ Assets/Standard Assets/ExplodedViews/MakeTriggers.cs
@@ -3,10 +3,15 @@ using System.Collections;
 
 public class MakeTriggers : MonoBehaviour {
 	// make any colliders under me triggers...
-	void Awake() {
+	void Start() {
 		foreach(Collider c in FindObjectsOfType(typeof(Collider)) as Collider[]) {
 			if (c.transform == transform || c.transform.IsChildOf(transform))
 			c.isTrigger = true;
 		}
+		/*
+		transform.DetachChildren();
+		Destroy(gameObject);
+		*/
 	}
+
 }
