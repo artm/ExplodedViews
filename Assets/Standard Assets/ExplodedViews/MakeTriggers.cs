f7e1b58fc7cbacf6ad4209097d836da5bb5fa235
Artem Baguinski <artm@v2.nl>
2011-11-13 16:31:53 +0100

    release children (crashes)

--- Assets/Standard Assets/ExplodedViews/MakeTriggers.cs
+++ Assets/Standard Assets/ExplodedViews/MakeTriggers.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
+using UnityEditor;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MakeTriggers : MonoBehaviour {
 	// make any colliders under me triggers...
@@ -8,10 +10,28 @@ public class MakeTriggers : MonoBehaviour {
 			if (c.transform == transform || c.transform.IsChildOf(transform))
 			c.isTrigger = true;
 		}
-		/*
+	}
+
+	[ContextMenu("Release children")]
+	void ReleaseTheCraken()
+	{
+		List<Transform> children = new List<Transform>();
+		foreach(Transform child in transform)
+		{
+			children.Add(child);
+		}
+
 		transform.DetachChildren();
-		Destroy(gameObject);
-		*/
+		DestroyImmediate(gameObject);
+
+		foreach(Transform child in children)
+		{
+			Object prefab = EditorUtility.GetPrefabParent(child.gameObject);
+			EditorUtility.ReplacePrefab(child.gameObject, prefab);
+			long memory = System.GC.GetTotalMemory(false);
+			System.GC.Collect();
+			Debug.Log("Cleaned up garbage: " + Pretty.Count(memory - System.GC.GetTotalMemory(false)));
+		}
 	}
 
 }
