3dca3b3f9a0d8abdcb7d9583c00a5ead140bda17
Artem Baguinski <artm@v2.nl>
2012-02-13 12:06:46 +0100

    Menu to create Prefs asset

--- Assets/Standard Assets/Editor/ExplodedPrefsInspector.cs
+++ Assets/Standard Assets/Editor/ExplodedPrefsInspector.cs
@@ -1,6 +1,7 @@
 using UnityEditor;
 using UnityEngine;
 using System.Collections;
+using System.IO;
 
 [CustomEditor(typeof(ExplodedPrefs))]
 public class ExplodedPrefsInspector : Editor
@@ -18,9 +19,32 @@ public class ExplodedPrefsInspector : Editor
 		GUILayout.Label( label );
 		if (GUILayout.Button( path )) {
 			string res = EditorUtility.OpenFolderPanel("Select " + label, path, "");
-			if (res != null && res != "")
+			if (res != null && res != "") {
 				path = res;
+				EditorUtility.SetDirty(prefs);
+			}
+
 		}
 	}
+
+	[MenuItem("Exploded Views/Prefs Asset")]
+	static void CreateResource()
+	{
+		if (EditorUtility.DisplayDialog("Create Exploded Prefs",
+		                                "Existing preferences will be overwritten. Are you sure?\n" +
+		                                "Preferences asset can be found in Project Window > Resources > ExplodedPrefs",
+		                                "Yes",
+		                                "No")) {
+			string res = Path.Combine("Assets","Resources");
+			if (!Directory.Exists(res))
+				Directory.CreateDirectory(res);
+	
+			ExplodedPrefs prefs = ScriptableObject.CreateInstance<ExplodedPrefs>();
+			prefs.importedPath = Path.GetFullPath("Bin");
+			AssetDatabase.CreateAsset(prefs, Path.Combine(res, "ExplodedPrefs.asset"));
+			AssetDatabase.Refresh();
+		}
+	}
+
 }
 
--- Assets/Standard Assets/ExplodedViews/ExplodedPrefs.cs
+++ Assets/Standard Assets/ExplodedViews/ExplodedPrefs.cs
@@ -1,31 +1,16 @@
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 using UnityEngine;
-using System.IO;
 
 public class ExplodedPrefs : ScriptableObject
 {
 	public string importedPath, incomingPath;
-	
-	public static ExplodedPrefs Instance()
-	{
-		 return Resources.Load("ExplodedPrefs") as ExplodedPrefs;
+
+	static ExplodedPrefs instance = null;
+	public static ExplodedPrefs Instance {
+		get {
+			if (instance == null)
+				instance = Resources.Load("ExplodedPrefs") as ExplodedPrefs;
+			return instance;
+		}
 	}
-	
-#if UNITY_EDITOR
-	[MenuItem("Assets/Exploded Prefs")]
-	static void CreateResource()
-	{
-		string res = Path.Combine("Assets","Resources");
-		if (!Directory.Exists(res))
-			Directory.CreateDirectory(res);
-		
-		ExplodedPrefs prefs = ScriptableObject.CreateInstance<ExplodedPrefs>();
-		prefs.importedPath = Path.GetFullPath("Bin");
-		AssetDatabase.CreateAsset(prefs, Path.Combine(res, "ExplodedPrefs.asset"));
-		AssetDatabase.Refresh();
-	}
-#endif
 }
 
