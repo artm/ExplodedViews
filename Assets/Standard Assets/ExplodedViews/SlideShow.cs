b4308d739e4abc3014b1e7cd3a60ef881b0756a3
Artem Baguinski <artm@v2.nl>
2012-03-20 12:47:27 +0100

    Disable preview mesh in run-time

--- Assets/Standard Assets/ExplodedViews/SlideShow.cs
+++ Assets/Standard Assets/ExplodedViews/SlideShow.cs
@@ -6,5 +6,16 @@ using Slice = ImportedCloud.Slice;
 public class SlideShow : MonoBehaviour
 {
 	public Slice[] slices;
+
+	// Awake is called before all Start()s
+	public void Awake() {
+		transform.FindChild("Full Cloud Preview").gameObject.SetActiveRecursively(false);
+	}
+
+	// Start is called after all Awake()s
+	public void Start() {
+		//...
+	}
+
 }
 
