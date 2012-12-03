55e985ecd8e327a3c578c65648fda4dfbc4610e9
Artem Baguinski (@ExploPC) <artm@v2.nl>
2012-12-03 15:51:46 +0100

    check if CloudMeshPool singleton still exists in its static API

--- Assets/Standard Assets/ExplodedViews/CloudMeshPool.cs
+++ Assets/Standard Assets/ExplodedViews/CloudMeshPool.cs
@@ -48,37 +48,47 @@ public class CloudMeshPool : MonoBehaviour {
 	
 	public static bool HasFreeMeshes {
 		get {
-			return singleton.freeMeshes.Count > 0;
+			return singleton != null && singleton.freeMeshes.Count > 0;
 		}
 	}
 	public static GameObject Get() {
-		if (singleton.freeMeshes.Count > 0) {
+		if (singleton != null && singleton.freeMeshes.Count > 0) {
 			return singleton.freeMeshes.Pop();
 		} else {
 			return null;
 		}
 	}
 	public static void Return(GameObject go) {
-		go.active = false;
-		go.transform.parent = singleton.transform;
-		singleton.freeMeshes.Push(go);
+		if (singleton != null) {
+			go.active = false;
+			go.transform.parent = singleton.transform;
+			singleton.freeMeshes.Push(go);
+		}
 	}
-	public static Material GetMaterial() { return singleton.material; }
+	public static Material GetMaterial() { return singleton != null ? singleton.material : null; }
 	
 	public static IEnumerator ReadFrom(CloudStream.Reader reader, GameObject go,
 	                                   float stride = 1f, int amount = -1, float scale = 1f) {
-		singleton.generator.Offset = 0;
-		yield return singleton.StartCoroutine(reader.ReadPointsAsync( singleton.generator, stride, amount ));
-		singleton.generator.ClearAfterOffset();
-		singleton.generator.Convert(go.GetComponent<MeshFilter>().sharedMesh, scale);
+		
+		if (singleton != null) {
+			singleton.generator.Offset = 0;
+			yield return singleton.StartCoroutine(reader.ReadPointsAsync( singleton.generator, stride, amount ));
+			singleton.generator.ClearAfterOffset();
+			singleton.generator.Convert(go.GetComponent<MeshFilter>().sharedMesh, scale);
+		}
+		
 	}
 
 	// filling the buffer...
 	public static IEnumerator ReadFrom(CloudStream.Reader reader, float stride = 1f, int amount = -1) {
-		yield return singleton.StartCoroutine(reader.ReadPointsAsync( singleton.generator, stride, amount ));
+		if (singleton != null)
+			yield return singleton.StartCoroutine(reader.ReadPointsAsync( singleton.generator, stride, amount ));
 	}
 	
 	public static GameObject PopBuffer() {
+		if (singleton == null)
+			return null;
+		
 		GameObject go = Get();
 		singleton.generator.ClearAfterOffset();
 		singleton.generator.Convert(go.GetComponent<MeshFilter>().sharedMesh);
@@ -87,14 +97,14 @@ public class CloudMeshPool : MonoBehaviour {
 		return go;
 	}
 	
-	public static int Capacity { get { return singleton.capacity; } }
-	public static int PointCapacity { get { return singleton.capacity * pointsPerMesh; } }
-	public static bool BufferFull { get { return singleton.generator.Full; } }
-	public static bool BufferEmpty { get { return singleton.generator.Empty; } }
+	public static int Capacity { get { return singleton != null ? singleton.capacity : 0; } }
+	public static int PointCapacity { get { return singleton != null ? singleton.capacity * pointsPerMesh : 0; } }
+	public static bool BufferFull { get { return singleton != null ? singleton.generator.Full : true; } }
+	public static bool BufferEmpty { get { return singleton != null ? singleton.generator.Empty : true; } }
 
 	public static int LoadedPointsCount {
 		get {
-			return (singleton.capacity - singleton.freeMeshes.Count) * pointsPerMesh;
+			return singleton != null ? (singleton.capacity - singleton.freeMeshes.Count) * pointsPerMesh : 0;
 		}
 	}
 	
